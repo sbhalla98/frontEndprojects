134a55d059663b3eb2649ecdf87e5a124cd9ec3a
sbhalla98 <sonalibhalla98@gmail.com>
2019-07-01 11:32:04 +0530

    second commit

--- nasa_project/nasa.js
+++ nasa_project/nasa.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-
+    $("#h1").click(function(){
+            location.reload(true);
+    });
 });
+
+$(document).ready(function(){
+    $("#text").css({width : "80%", position : "relative",top: "250px" , left : "10%",padding :"0.5%"});
+});
+/*
+var xhr = new XMLHttpRequest();
+xhr.open('GET','https://api.nasa.gov/planetary/apod?api_key=“Photo of the Day”');
+xhr.send();
+xhr.onreadystatechange = function(){
+    console.log(this.responseText);
+    
+};
+*/
+$(window).ready(function(){
+    
+            $(".loader").hide();
+ 
+});
+/*
+var xhr = new XMLHttpRequest();
+xhr.open('GET','https://api.nasa.gov/planetary/apod?api_key=“Photo of the Day”');
+xhr.send();
+xhr.onreadystatechange = function(){
+    console.log(this.responseText);
+    
+};
+*/
