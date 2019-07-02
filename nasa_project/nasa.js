c4f058c4acd0ab13ea945a3741b646751d07dffe
sbhalla98 <sonalibhalla98@gmail.com>
2019-07-02 10:59:46 +0530

    third commit backend done

--- nasa_project/nasa.js
+++ nasa_project/nasa.js
@@ -7,26 +7,50 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#text").css({width : "80%", position : "relative",top: "250px" , left : "10%",padding :"0.5%"});
 });
-/*
+function f1(){
+
+}
+
 var xhr = new XMLHttpRequest();
-xhr.open('GET','https://api.nasa.gov/planetary/apod?api_key=“Photo of the Day”');
+function f1(){
+xhr.open('GET','https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',true);
 xhr.send();
-xhr.onreadystatechange = function(){
-    console.log(this.responseText);
-    
+xhr.onreadystatechange = function(){ 
+    if(this.readyState==4){
+    var x = JSON.parse(this.responseText);
+   document.body.style.backgroundImage = "url("+x.url+")";
+   document.getElementsByClassName("loader")[0].style.display = "none";
+    }
 };
-*/
-$(window).ready(function(){
-    
-            $(".loader").hide();
- 
+}
+
+$("input").on("keyup",function(){
+    console.log("done");
+        if(event.keyCode == 13){
+            var xhr = new XMLHttpRequest();
+            var y = document.getElementById("text").value;
+            console.log(y);
+            xhr.open('GET','https://images-api.nasa.gov/search?q='+y);
+            xhr.send();
+            xhr.onreadystatechange = function(){
+                if(this.readyState == 4){
+                    var x = JSON.parse(this.responseText);
+                  
+                    var images = x.collection.items;
+
+                    images.forEach(function(value,index){
+                                var links = value.links;
+                                links.forEach(function(value,index){
+                                    console.log(value.href);
+                                    document.getElementById("images").innerHTML += '<img class="img.thumbnail i rounded" src='+ value.href +'></img>';
+                                });
+                    });
+                        
+                    };
+                   
+                }
+
+            };
+        
 });
-/*
-var xhr = new XMLHttpRequest();
-xhr.open('GET','https://api.nasa.gov/planetary/apod?api_key=“Photo of the Day”');
-xhr.send();
-xhr.onreadystatechange = function(){
-    console.log(this.responseText);
-    
-};
-*/
+
